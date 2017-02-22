ec3e94d339038f6fa12ae4884a670bfd56a8ad15
eggcaker <eggcaker@gmail.com>
2017-02-22 09:24:53 +0800

    cleanup bins

--- commands/bin/c.get_pref_by_key.sh
+++ commands/bin/c.get_pref_by_key.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+KEY="${1:-group_myself_account_key}"
 adb shell "su -c 'mount -o rw,remount /system'"
 adb shell "su -c 'cp /data/data/cc.pacer.androidapp/shared_prefs/pref_pacer.xml /mnt/sdcard/pref_pacer.xml'"
 adb shell "su -c 'mount -o ro,remount /system'"
 adb pull /mnt/sdcard/pref_pacer.xml .
-cat pref_pacer.xml|grep "${1}"|sed -e 's/&quot;/"/g'|sed -e 's/<string name="'"${1}"'">//g'|sed -e 's/<\/string>//g'|jq ''
+cat pref_pacer.xml|grep "${KEY}"|sed -e 's/&quot;/"/g'|sed -e 's/<string name="'"${KEY}"'">//g'|sed -e 's/<\/string>//g'|jq ''
 rm pref_pacer.xml
--- commands/bin/c.pb
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./pastebin.py $*
