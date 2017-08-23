89f60dc9c3832c706acf9659a328f633d78fe8fa
eggcaker <eggcaker@gmail.com>
2017-08-23 16:47:19 +0800

    update script

--- omf/init.fish
+++ omf/init.fish
@@ -1,7 +1,13 @@
 # TODO: rewrite this based on fish way ??
 source ~/.aliases
+source ~/.fishrc.local
 
 # bin
 set -gx PATH $PATH ~/bin
+set -gx ANDROID_HOME  /usr/local/Cellar/android-sdk/24.4.1_1
+set -gx ANDROID_BIN  $ANDROID_HOME/platform-tools $ANDROID_HOME/tools/
+set -gx PATH $PATH $ANDROID_BIN
+set -gx NODE_PATH  "/Users/eggcaker/.nodenv/versions/8.3.0/lib/node_modules"
+
 
 set -gx  LC_ALL en_US.UTF-8
