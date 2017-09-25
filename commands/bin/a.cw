fbf596b591cb876482fd09815f2bbfa61d5570d5
eggcaker <eggcaker@gmail.com>
2017-09-25 08:57:23 +0800

    update androis sdk directory

--- commands/bin/a.cw
+++ commands/bin/a.cw
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-adb forward tcp:4444 localabstract:/adb-hub
-adb connect localhost:4444
+adb tcpip 5555
+adb connect 10.1.13.59 
--- omf/init.fish
+++ omf/init.fish
@@ -4,7 +4,7 @@ source ~/.fishrc.local
 
 # bin
 set -gx PATH $PATH ~/bin
-set -gx ANDROID_HOME  /usr/local/Cellar/android-sdk/24.4.1_1
+set -gx ANDROID_HOME cd /usr/local/Caskroom/android-sdk/3859397,26.0.1/
 set -gx ANDROID_BIN  $ANDROID_HOME/platform-tools $ANDROID_HOME/tools/
 set -gx PATH $PATH $ANDROID_BIN
 set -gx NODE_PATH  "/Users/eggcaker/.nodenv/versions/8.3.0/lib/node_modules"
