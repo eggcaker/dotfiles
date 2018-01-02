1d86eb38faca3f0b5b309b074150e7d8338f2443
eggcaker <eggcaker@gmail.com>
2018-01-02 12:59:22 +0800

    add publish scripts

--- bin/c.d
+++ bin/c.d
@@ -10,7 +10,7 @@
 
 (if (or (= 0 (length argv)) (<= el/tt 0))
     (message (number-to-string (float-time)))
-  (let ((seconds (if (> el/tt 6489546000 ) el/tt (/ el/tt 1000))))
+  (let ((seconds (if (> el/tt 6489546000 )  (/ el/tt 1000.0) el/tt )))
     (message (format-time-string
               "%Y-%m-%d %H:%M:%S"
-              (seconds-to-time (* seconds 1000))))))
+              (seconds-to-time  seconds )))))
