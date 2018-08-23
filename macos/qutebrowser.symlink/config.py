62469fd23f37a3a56bf53595d6e7c236fbf77726
eggcaker <eggcaker@gmail.com>
2018-08-23 19:15:17 +0800

    Daily commit 2018-08-23

--- macos/qutebrowser.symlink/config.py
+++ macos/qutebrowser.symlink/config.py
@@ -42,20 +42,8 @@ c.aliases = {
 # Always restore open sites when qutebrowser is reopened.
 c.auto_save.session = True
 
-# Foreground color of the URL in the statusbar on successful load
-# (https).
-c.colors.statusbar.url.success.https.fg = "white"
-
-# Background color of unselected tabs.
-c.colors.tabs.even.bg = "silver"
-c.colors.tabs.odd.bg = "gainsboro"
-
-# Foreground color of unselected tabs.
-c.colors.tabs.even.fg = "#666666"
-c.colors.tabs.odd.fg = c.colors.tabs.even.fg
-
 # The height of the completion, in px or as percentage of the window.
-c.completion.height = "20%"
+c.completion.height = "25%"
 
 # Move on to the next part when there's only one possible completion
 # left.
@@ -106,9 +94,9 @@ c.downloads.location.prompt = False
 
 # The editor (and arguments) to use for the `open-editor` command. `{}`
 # gets replaced by the filename of the file to be edited.
-c.editor.command = ["termite", "-e", "vim '{}'"]
+c.editor.command = ["urxvt", "-e", "vim '{}'"]
 
-monospace = "13pt 'Bok MonteCarlo'"
+monospace = "14pt 'Fira code'"
 
 # Font used in the completion categories.
 c.fonts.completion.category = f"bold {monospace}"
@@ -224,6 +212,155 @@ c.url.start_pages = "https://github.com/"
 #   * `{backend}`: Either ''webkit'' or ''webengine''
 #   * `{private}` : Indicates when private mode is enabled.
 c.window.title_format = "{private}{perc}{title}{title_sep}qutebrowser"
-c.window.hide_decoration = True
+c.window.hide_decoration = False #True
 
 c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+
+
+
+c.colors.completion.fg = "#f3f3f3"
+#c.colors.completion.bg = "#b2b2b2"
+
+# c.colors.completion.alternate.bg = "#282828"
+
+c.colors.completion.category.fg = "gray"
+
+c.colors.completion.category.border.top = "#282828"
+
+c.colors.completion.category.border.bottom = c.colors.completion.category.border.top
+
+c.colors.completion.item.selected.fg = "#1d2021"
+
+c.colors.completion.item.selected.bg = "#a89984"
+
+c.colors.completion.item.selected.border.top = "#282828"
+
+c.colors.completion.item.selected.border.bottom = c.colors.completion.item.selected.border.top
+
+c.colors.completion.match.fg = "#689d6a"
+
+c.colors.completion.scrollbar.fg = c.colors.completion.fg
+
+# c.colors.completion.scrollbar.bg = c.colors.completion.bg
+
+c.colors.statusbar.normal.fg = "#a89984"
+
+c.colors.statusbar.normal.bg = "#252525"
+
+c.colors.statusbar.private.fg = c.colors.statusbar.normal.fg
+
+c.colors.statusbar.private.bg = "#666666"
+
+c.colors.statusbar.insert.fg = c.colors.statusbar.normal.fg
+
+c.colors.statusbar.insert.bg = "#252525"
+
+c.colors.statusbar.command.fg = c.colors.statusbar.normal.fg
+
+c.colors.statusbar.command.bg = c.colors.statusbar.normal.bg
+
+c.colors.statusbar.command.private.fg = c.colors.statusbar.private.fg
+
+c.colors.statusbar.command.private.bg = c.colors.statusbar.private.bg
+
+c.colors.statusbar.caret.fg = c.colors.statusbar.normal.fg
+
+c.colors.statusbar.caret.bg = "#a89984"
+
+c.colors.statusbar.caret.selection.fg = c.colors.statusbar.normal.fg
+
+c.colors.statusbar.caret.selection.bg = "#458588"
+
+c.colors.statusbar.progress.bg = "#83a598"
+
+c.colors.statusbar.url.fg = c.colors.statusbar.normal.fg
+
+c.colors.statusbar.url.success.http.fg = "#a89984"
+
+c.colors.statusbar.url.success.https.fg = "#689d6a"
+
+c.colors.statusbar.url.error.fg = "#fb4934"
+
+c.colors.statusbar.url.warn.fg = "#b16286"
+
+c.colors.statusbar.url.hover.fg = "#458588"
+
+c.colors.tabs.odd.fg = "#a89984"
+
+c.colors.tabs.odd.bg = "#1d2021"
+
+c.colors.tabs.even.fg = "#a89984"
+
+c.colors.tabs.even.bg = "#1d2021"
+
+c.colors.tabs.selected.odd.fg = "#8ec07c"
+
+c.colors.tabs.selected.odd.bg = "#282828"
+
+c.colors.tabs.selected.even.fg = c.colors.tabs.selected.odd.fg
+
+c.colors.tabs.selected.even.bg = c.colors.tabs.selected.odd.bg
+
+c.colors.tabs.bar.bg = "#1d2021"
+
+c.colors.tabs.indicator.start = "#ebdbb2"
+
+c.colors.tabs.indicator.stop = "#b16286"
+
+c.colors.tabs.indicator.error = "#d65d0e"
+
+c.colors.tabs.indicator.system = "rgb"
+
+c.colors.hints.fg = "#fbf1c7"
+
+c.colors.hints.bg = "#665c54"
+
+c.colors.hints.match.fg = "#fe8019"
+
+c.colors.downloads.bar.bg = "#3c3836"
+
+c.colors.downloads.start.fg = "#ebdbb2"
+
+c.colors.downloads.start.bg = "#d65d0e"
+
+c.colors.downloads.stop.fg = c.colors.downloads.start.fg
+
+c.colors.downloads.stop.bg = "#689d6a"
+
+c.colors.downloads.system.fg = "rgb"
+
+c.colors.downloads.system.bg = "rgb"
+
+c.colors.downloads.error.fg = "#b1a191"
+
+c.colors.downloads.error.bg = "#d79921"
+
+c.colors.webpage.bg = "#fbf1c7"
+
+c.colors.keyhint.fg = "#d33682"
+
+c.colors.keyhint.suffix.fg = "#2aa198"
+
+c.colors.messages.error.fg = "#fdf6e3"
+
+c.colors.messages.error.bg = "#dc322f"
+
+c.colors.messages.error.border = "#fdf6e3"
+
+c.colors.messages.warning.fg = "#859900"
+
+c.colors.messages.warning.bg = "#fdf6e3"
+
+c.colors.messages.warning.border = "#d47300"
+
+c.colors.messages.info.fg = "#a89984"
+
+c.colors.messages.info.bg = "#1d2021"
+
+c.colors.messages.info.border = "#202020"
+
+c.colors.prompts.fg = "#a89984"
+
+c.colors.prompts.bg = "#282828"
+
+c.colors.prompts.selected.bg = "#282828"
