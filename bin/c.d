f96fb2ff26f5d9ae72776996f8bc27686e5ee9d6
eggcaker <eggcaker@gmail.com>
2019-01-19 19:15:08 +0800

    Daily commit 2019-01-19

--- bin/c.d
+++ bin/c.d
@@ -6,8 +6,13 @@ import sys
 
 import pendulum as p
 
-if len(sys.argv) == 2:
+if len(sys.argv) >= 2:
     DATE_TIME_IN_SECONDS = int(sys.argv[1])
+    if (len(sys.argv) == 3):
+        OFFSET = int(sys.argv[2])
+        if (OFFSET  != 0) :
+            DATE_TIME_IN_SECONDS += OFFSET*3600
+
     print(p.from_timestamp(DATE_TIME_IN_SECONDS).strftime('%Y-%m-%d %H:%M:%S'))
 else:
     print(p.now().int_timestamp)
