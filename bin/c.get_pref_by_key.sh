17fcb0282d3a67ec84f8924643b894d102f9fcdc
eggcaker <eggcaker@gmail.com>
2020-03-12 06:43:19 +0800

    cleanup scripts :(

--- bin/c.get_pref_by_key.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-KEY="${1:-group_myself_account_key}"
-adb shell "su -c 'mount -o rw,remount /system'"
-adb shell "su -c 'cp /data/data/cc.pacer.androidapp/shared_prefs/pref_pacer.xml /mnt/sdcard/pref_pacer.xml'"
-adb shell "su -c 'mount -o ro,remount /system'"
-adb pull /mnt/sdcard/pref_pacer.xml .
-cat pref_pacer.xml | grep "${KEY}" | sed -e 's/&quot;/"/g' | awk -F'">' '{print $2}' | awk -F\" '{print $1}'
-#rm pref_pacer.xml
